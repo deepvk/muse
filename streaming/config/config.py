08fefd17e6cf04d254ccb13805d2d88f3690536a
maks00170 <maks.groshev4@mail.ru>
2023-12-30 19:45:23 +0300

    ref comment config

--- streaming/config/config.py
+++ streaming/config/config.py
@@ -5,20 +5,20 @@ from pathlib import Path
 @dataclass
 class ConverterConfig:
     # WEIGHTS LOAD
-    weights_dir            : Path  = Path("/app/streaming/weights")      # Path to the directory where the model weight files are stored. 
-    weights_LSTM_filename  : str   = "weight_LSTM.pt"                    # This is the filename for the LSTM weights file.
-    weights_conv_filename  : str   = "weight_conv.pt"                    # This is the filename for the without CNN weights file.
-    gdrive_weights_LSTM_id : str   = "18jT2TYffdRD1fL7wecAiM5nJPM_OKpNB" # This is the Google Drive ID for the LSTM weights file.
-    gdrive_weights_conv_id : str   = "1VO07OYbsnCuEJYRSuA8HhjlQnx6dbWX7" # This is the Google Drive ID for the CNN weights file.
+    weights_dir            : Path = Path("/app/streaming/weights")      # Path to the directory where the model weight files are stored. 
+    weights_LSTM_filename  : str  = "weight_LSTM.pt"                    # This is the filename for the LSTM weights file.
+    weights_conv_filename  : str  = "weight_conv.pt"                    # This is the filename for the without CNN weights file.
+    gdrive_weights_LSTM_id : str  = "18jT2TYffdRD1fL7wecAiM5nJPM_OKpNB" # This is the Google Drive ID for the LSTM weights file.
+    gdrive_weights_conv_id : str  = "1VO07OYbsnCuEJYRSuA8HhjlQnx6dbWX7" # This is the Google Drive ID for the CNN weights file.
     
     # MODEL OPTIONS
-    original_model_src : str   = "/app/separator/model"              # This parameter represents the source directory of the original model.
-    original_model_dst : str   = "/app/streaming/model"              # This parameter represents the destination directory of the original model.
-    model_py_module    : str   = "model.PM_Unet"                     # This is the python module where the model is defined
-    model_class_name   : str   = "Model_Unet"                        # The name of the model class.
-    tflite_model_dst   : str   = "tflite_model"                      # This is the destination directory for the TFLite model.
-    sample_rate        : int   = 44100                               # Sample rate track
-    segment_duration   : float = 1.0                                 # This parameter represents the duration of the audio segments that the model will process.
+    original_model_src : str   = "/app/separator/model" # This parameter represents the source directory of the original model.
+    original_model_dst : str   = "/app/streaming/model" # This parameter represents the destination directory of the original model.
+    model_py_module    : str   = "model.PM_Unet"        # This is the python module where the model is defined
+    model_class_name   : str   = "Model_Unet"           # The name of the model class.
+    tflite_model_dst   : str   = "tflite_model"         # This is the destination directory for the TFLite model.
+    sample_rate        : int   = 44100                  # Sample rate track
+    segment_duration   : float = 1.0                    # This parameter represents the duration of the audio segments that the model will process.
 
 
 @dataclass
